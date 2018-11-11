814ea26cce055f2e750af533e9f24d2e05b0e9c3
= <=>
2018-11-11 10:55:52 +0000

    fixed bootstrapper

--- src/SportRanker.Feeds.SportRadar.NBA.App/AppBootstrapper.cs
+++ src/SportRanker.Feeds.SportRadar.NBA.App/AppBootstrapper.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Autofac;
+using SportRanker.Feeds.SportRadar.NBA.Application;
+using SportRanker.Feeds.SportRadar.NBA.Infrastructure;
+using SportRanker.Feeds.SportRadar.NBA.Interfaces;
 
 namespace SportRanker.Feeds.SportRadar.NBA.App
 {
-    class AppBootstrapper
+    public class AppBootstrapper
     {
+        public static IContainer Bootstrap()
+        {
+            var builder = new ContainerBuilder();
+
+            builder
+                .RegisterType<FeedProcessor>()
+                .As<IFeedProcessor>();
+
+            builder
+                .RegisterType<FeedConsumer>()
+                .As<IFeedConsumer>();
+
+            builder
+                .RegisterType<Publisher>()
+                .As<IPublisher>();
+
+            return builder.Build();
+        }
     }
 }
