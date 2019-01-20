781ae89db160a7d6f1c55ecb0732e3a6a70c64f3
= <=>
2019-01-20 19:14:23 +0000

    run cron job once a week

--- src/SportRanker.Feeds.SportRadar.NBA.TestApp/AppBootstrapper.cs
+++ src/SportRanker.Feeds.SportRadar.NBA.TestApp/AppBootstrapper.cs
@@ -20,7 +20,7 @@ namespace SportRanker.Feeds.SportRadar.NBA.TestApp
                 .As<IFeedConsumer>();
 
             builder
-                .RegisterType<FilePublisher>()
+                .RegisterType<Publisher>()
                 .As<IPublisher>();
 
             builder
